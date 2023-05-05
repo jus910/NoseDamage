29df012602ca5d1473630d368bd3bc1b039ccf9a
Jason Mohabir <jmohabir30@stuy.edu>
2023-05-04 21:42:33 -0400

    basic flask

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+from flask import session, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
